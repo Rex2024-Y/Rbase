29bd4979cb5df5c002a9b62522226aadf56b32f9
Dingyeran <289401595@qq.com>
2024-06-28 17:57:16 +0800

    线程池管理

---
+++ baselibrary/src/main/java/com/zg/baselibrary/executor/DThreadExecutor.kt
@@ -0,0 +1,45 @@
+package com.zg.baselibrary.executor
+
+import android.os.Handler
+import android.os.Looper
+import java.util.concurrent.Executors
+
+class DThreadExecutor {
+
+
+    private val mainHandler = Handler(Looper.getMainLooper())
+
+    // IO线程池
+    val ioExecutor =
+        Executors.newFixedThreadPool(3)
+    // 单线程
+    val singleExecutor = Executors.newSingleThreadExecutor();
+    fun executeUi(command: Runnable) {
+        if (Looper.myLooper() == Looper.getMainLooper()) {
+            // 当前线程是UI线程，直接运行
+            command.run()
+        } else {
+            // 切换到UI线程执行
+            mainHandler.post(command)
+        }
+    }
+
+    fun executeIo(command: Runnable) {
+        ioExecutor.execute(command)
+    }
+
+
+
+    fun executeOne(command: Runnable) {
+        singleExecutor.execute(command)
+    }
+
+    companion object {
+
+        private const val TAG = "UiThreadExecutor"
+        fun getInstance() = Holder.mInstance
+        object Holder {
+            val mInstance: DThreadExecutor = DThreadExecutor()
+        }
+    }
+}
